c5f14c146b864ff71a6da22239c72df764ea7c7a
Rachel Wanke <rkhorne09@gmail.com>
2021-03-17 00:52:43 -0400

    setting up a new portfolio page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//const path = require('path');
+const nodemailer = require('nodemailer')
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+//Setting up routes for pages.
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
