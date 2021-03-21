9112341b24913ff37b72c09f59b192ac33dca234
Rachel Wanke <rkhorne09@gmail.com>
2021-03-21 01:14:05 -0400

    adding edits to navbar and starting to format cards for skills

--- public/js/title.js
+++ public/js/title.js
@@ -43,5 +43,17 @@ anime.timeline({loop: 1})
   })
 
 
+  window.onscroll = function() {myFunction()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
   
   
